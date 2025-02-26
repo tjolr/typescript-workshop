6b6cb73396e1bcbd4d8810c5dab064c3d109c6ef
Tjøl Ravndal <tjol.ravndal@bekk.no>
2025-02-26 01:19:40 +0100

    Add oppsett folder with link to Pocock, and ts-version-select

---
+++ src/0-oppsett/01-intro-matt-pocock.ts
@@ -0,0 +1,2 @@
+// Intro ved Matt Pocock
+// https://www.totaltypescript.com/workshops/typescript-pro-essentials/kickstart-your-typescript-set-up/typescripts-relationship-with-javascript
---
+++ src/0-oppsett/02-starting.ts
@@ -0,0 +1,6 @@
+/* 
+    Noen nyttige knagger:
+    - Velge riktig typescript versjon i VSCode
+    - Konfigurere typescript i package.json
+    - Reload TS-serveren
+*/
